f76751eba41592f84edc7f5a151aeb93773d8396
x1bViolettcs <x1bviolet@gmail.com>
2026-04-09 15:17:07 +0500

    Format NameLineElement DependencyProperties

--- Limbus Registry/WPF Controls/Specific Elements/Name Line Element (Skills & Passives).xaml.cs
+++ Limbus Registry/WPF Controls/Specific Elements/Name Line Element (Skills & Passives).xaml.cs
@@ -9,42 +9,40 @@ namespace LCLocalizationInterface.LimbusRegistry
     /// </summary>
     public partial class NameLineElement : UserControl
     {
+        public string DisplayedName { get => (string)GetValue(DisplayedNameProperty); set => SetValue(DisplayedNameProperty, value); }
         public static readonly DependencyProperty DisplayedNameProperty = RegisterProperty<NameLineElement, string>(DefaultValue: "");
+        
+        public double NameMaximumWidth { get => (double)GetValue(NameMaximumWidthProperty); set => SetValue(NameMaximumWidthProperty, value); }
         public static readonly DependencyProperty NameMaximumWidthProperty = RegisterProperty<NameLineElement, double>(DefaultValue: 500.0);
+        
+        public AffinityName Affinity { get => (AffinityName)GetValue(AffinityProperty); set => SetValue(AffinityProperty, value); }
         public static readonly DependencyProperty AffinityProperty = RegisterProperty<NameLineElement, AffinityName>(DefaultValue: AffinityName.None, PropertyChangedEvent: FillColorSetter);
-
-        public static readonly DependencyProperty DropShadowEffect_DepthProperty = RegisterProperty<NameLineElement, double>(DefaultValue: 4.0);
-        public static readonly DependencyProperty DropShadowEffect_DirectionProperty = RegisterProperty<NameLineElement, double>(DefaultValue: -40.0);
-
         private static void FillColorSetter(DependencyObject Sender, DependencyPropertyChangedEventArgs Args)
         {
             (Sender as NameLineElement)!.FillColor = ToSolidColorBrush(GetAffinityColor($"{Args.NewValue}"));
         }
 
+
+        public double DropShadowEffect_Depth { get => (double)GetValue(DropShadowEffect_DepthProperty); set => SetValue(DropShadowEffect_DepthProperty, value); }
+        public static readonly DependencyProperty DropShadowEffect_DepthProperty = RegisterProperty<NameLineElement, double>(DefaultValue: 4.0);
+        
+        public double DropShadowEffect_Direction { get => (double)GetValue(DropShadowEffect_DirectionProperty); set => SetValue(DropShadowEffect_DirectionProperty, value); }
+        public static readonly DependencyProperty DropShadowEffect_DirectionProperty = RegisterProperty<NameLineElement, double>(DefaultValue: -40.0);
+
+
         #region Technical DependencyProperties
+        public ImageSource CornerImageSource { get => (ImageSource)GetValue(CornerImageSourceProperty); set => SetValue(CornerImageSourceProperty, value); }
         public static readonly DependencyProperty CornerImageSourceProperty = RegisterProperty<NameLineElement, ImageSource>();
+        
+        public SolidColorBrush FillColor { get => (SolidColorBrush)GetValue(FillColorProperty); set => SetValue(FillColorProperty, value); }
         public static readonly DependencyProperty FillColorProperty = RegisterProperty<NameLineElement, SolidColorBrush>(DefaultValue: ToSolidColorBrush("#9f6a3a"));
+        
+        public double FillColorRightOffsetReduction { get => (double)GetValue(FillColorRightOffsetReductionProperty); set => SetValue(FillColorRightOffsetReductionProperty, value); }
         public static readonly DependencyProperty FillColorRightOffsetReductionProperty = RegisterProperty<NameLineElement, double>();
         #endregion
 
 
-        public void RefreshNameRichText() => this.FindTypeNameFromTemplate<TMProEmitter>("NameDisplaying")!.RefreshRichText();
-
-
-        public string DisplayedName { get => (string)GetValue(DisplayedNameProperty); set => SetValue(DisplayedNameProperty, value); }
-        public double NameMaximumWidth { get => (double)GetValue(NameMaximumWidthProperty); set => SetValue(NameMaximumWidthProperty, value); }
-        public AffinityName Affinity { get => (AffinityName)GetValue(AffinityProperty); set => SetValue(AffinityProperty, value); }
-
-        public double DropShadowEffect_Depth { get => (double)GetValue(DropShadowEffect_DepthProperty); set => SetValue(DropShadowEffect_DepthProperty, value); }
-        public double DropShadowEffect_Direction { get => (double)GetValue(DropShadowEffect_DirectionProperty); set => SetValue(DropShadowEffect_DirectionProperty, value); }
-
-
-
-        #region Technical Properties used in ControlTemplate Bindings
-        public ImageSource CornerImageSource { get => (ImageSource)GetValue(CornerImageSourceProperty); set => SetValue(CornerImageSourceProperty, value); }
-        public SolidColorBrush FillColor { get => (SolidColorBrush)GetValue(FillColorProperty); set => SetValue(FillColorProperty, value); }
-        public double FillColorRightOffsetReduction { get => (double)GetValue(FillColorRightOffsetReductionProperty); set => SetValue(FillColorRightOffsetReductionProperty, value); }
-        #endregion
+        public void RefreshNameRichText() => this.FindTypeNameFromTemplate<TMProEmitter>("NameDisplaying")?.RefreshRichText();
     }
 
     namespace LimbusIValueConverters
